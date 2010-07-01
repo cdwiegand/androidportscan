473f487cbc892bcdaab5a226e3d5bf298330c9d7
Chris Wiegand <chris.wiegand@gmail.com>
2010-07-01 00:33:56 -0600

    changed progress dialog

--- src/com/wiegandfamily/portscan/ScanConfig.java
+++ src/com/wiegandfamily/portscan/ScanConfig.java
@@ -2,38 +2,52 @@ package com.wiegandfamily.portscan;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ScanConfig extends Activity {
+	private static final String LOGTAG = "ScanConfig";
+
 	private static final int MENU_ABOUT = 4;
 	private static final int MENU_RERUN = 6;
 	private static final int MENU_EXIT = 5;
+
+	protected ProgressDialog pd = null;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.main);
+		// setContentView(R.layout.main);
 		setContentView(R.layout.results);
 		run();
 	}
-	
+
 	protected void run() {
+		if (pd == null) {
+			pd = new ProgressDialog(this);
+			pd.show();
+		}
+
 		TextView txtBox = (TextView) findViewById(R.id.TextView02);
 		txtBox.setText("");
-		
+
 		NetworkScanner scanner = new NetworkScanner();
 		scanner.setHandler(handler);
 		scanner.setPortList(NetworkScanner.PORTLIST_COMMON);
 		Thread thread = new Thread(scanner);
 		thread.start();
 	}
-	
+
 	/* Creates the menu items */
 	public boolean onCreateOptionsMenu(Menu menu) {
 		menu.add(0, MENU_ABOUT, 0, R.string.menu_about).setIcon(
@@ -44,7 +58,7 @@ public class ScanConfig extends Activity {
 				R.drawable.ic_menu_close_clear_cancel);
 		return true;
 	}
-	
+
 	/* Handles menu item selections */
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -60,10 +74,24 @@ public class ScanConfig extends Activity {
 		}
 		return false;
 	}
-	
+
 	public void showAbout() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage("Port Scanner\nCopyright 2010 by Chris Wiegand\n\nCovered by MIT license");
+
+		String versionNumber = "";
+		try {
+			PackageManager manager = getApplicationContext()
+					.getPackageManager();
+			PackageInfo info = manager.getPackageInfo(getApplicationContext()
+					.getPackageName(), 0);
+			versionNumber = info.versionName;
+		} catch (Exception e) {
+			Log.e(LOGTAG, e.getMessage());
+		}
+		builder
+				.setMessage("Port Scanner "
+						+ versionNumber
+						+ "\nCopyright 2010 by Chris Wiegand\n\nCovered by MIT license");
 		AlertDialog alert = builder.create();
 		alert.show();
 	}
@@ -77,19 +105,25 @@ public class ScanConfig extends Activity {
 			case NetworkScanner.MSG_DONE:
 				txtBox = (TextView) findViewById(R.id.TextView01);
 				txtBox.setText(getAppString(R.string.results));
+				if (pd != null) {
+					pd.dismiss();
+					pd = null;
+				}
 				break;
 			case NetworkScanner.MSG_UPDATE:
-				txtBox = (TextView) findViewById(R.id.TextView01);
-				txtBox.setText(getAppString(R.string.scanning) + " " + msg.obj.toString());
+				if (pd != null)
+					pd.setMessage(getAppString(R.string.scanning) + " "
+							+ msg.obj.toString());
 				break;
 			case NetworkScanner.MSG_FOUND:
 				txtBox = (TextView) findViewById(R.id.TextView02);
-				txtBox.setText(txtBox.getText().toString() + "\n" + msg.obj.toString());
+				txtBox.setText(txtBox.getText().toString() + "\n"
+						+ msg.obj.toString());
 				break;
 			}
 		}
 	};
-	
+
 	protected String getAppString(int id) {
 		return getApplicationContext().getResources().getString(id);
 	}
